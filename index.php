3b2a152b77e7f5d208012486d30b47f270799ae2
nam <naph47@gmail.com>
2016-09-29 19:01:11 -0700

    update

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, minimum-scale=1.0, initial-scale=1.0, maximum-scale=1.0">
+    <script src="bower_components/webcomponentsjs/webcomponents.min.js"></script>
+    <link rel="import" href="bower_components/polymer/polymer.html">
+    <link rel="import" href="bower_components/paper-toolbar/paper-toolbar.html">
+    <link rel="import" href="bower_components/paper-styles/paper-styles.html">
+</head>
+<body>
+<paper-toolbar>
+    <paper-icon-button icon="menu"></paper-icon-button>
+    <span class="title">Title</span>
+    <paper-icon-button icon="refresh"></paper-icon-button>
+    <paper-icon-button icon="add">+</paper-icon-button>
+</paper-toolbar>
+</body>
+</html>
