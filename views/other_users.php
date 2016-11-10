ea68995ccf73242bf771d57b4e644f8903597b91
nam <naph47@gmail.com>
2016-11-09 21:42:03 -0800

    update other user

--- views/other_users.php
+++ views/other_users.php
@@ -3,6 +3,57 @@
         <div class="col-xs-12">
             <paper-card class="col-xs-12" heading="Users from other sites" alt="Users from other sites">
                 <div class="card-content">
+                    <p>Users from <a href="http://myxaxi.net/service/external_users.php">http://myxaxi.net/service/external_users.php</a></p>
+                    <?php
+                    $ch = curl_init();
+
+                    curl_setopt_array($ch, array(
+                        CURLOPT_URL => 'http://myxaxi.net/service/external_users.php',
+                        CURLOPT_RETURNTRANSFER => true,
+                        CURLOPT_POST => true,
+                        CURLOPT_FOLLOWLOCATION => true
+                    ));
+
+                    $output = curl_exec($ch);
+                    $output = json_decode($output);
+                    ?>
+                    <table class="table table-bordered">
+                        <thead>
+                        <?php
+                        foreach ($output[0] as $key => $value) {
+                            ?>
+                            <th>
+                                <?php
+                                echo $key;
+                                ?>
+                            </th>
+                            <?php
+                        }
+                        ?>
+                        </thead>
+                        <tbody>
+                        <?php
+                        foreach ($output as $row) {
+
+                            ?>
+                            <tr>
+                                <?php
+                                foreach ($row as $key => $value) {
+                                    ?>
+                                    <td><?php
+                                        echo $value;
+                                        ?></td>
+                                    <?php
+                                }
+                                ?>
+                            </tr>
+                            <?php
+                        }
+                        ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="card-content">
                     <p>Users from <a href="http://konstella.me/access.php">http://konstella.me/access.php</a></p>
                     <?php
                     $ch = curl_init();
@@ -15,13 +66,7 @@
                     ));
 
                     $output = curl_exec($ch);
-                    //                    echo $output;
                     $output = json_decode($output);
-                    //                    print_r($output);
-                    //                    foreach ($output[0] as $key => $value) {
-                    //                        print_r($key);
-                    //                    }
-
                     ?>
                     <table class="table table-bordered">
                         <thead>
